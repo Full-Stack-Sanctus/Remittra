6ebe689685c7246d7fe775d5c85156f12511dcc6
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-01-23 21:14:32 +0000

    Pushing commit

--- lib/supabaseClient.tsx
+++ lib/supabaseClient.tsx
@@ -9,9 +9,9 @@ export const supabase = createClient<Database>(
 
 */
 
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? (() => { throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL") })(),
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? (() => { throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY") })()
+)
