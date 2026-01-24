817d2c750051c3965c5f02ffe82c8d57a4aaf0dc
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-01-24 00:38:53 +0000

    Pushing commit

--- app/api/ajos/join/route.ts
+++ app/api/ajos/join/route.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseServer";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
