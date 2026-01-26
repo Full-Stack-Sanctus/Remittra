9f949c53610a250a084b20af0d86d1f118d15066
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-01-26 17:14:11 +0000

    Pushing commit

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,11 @@
 import WalletSection from "@/components/wallet/WalletSection";
 import AjoSection from "@/components/ajo/AjoSection";
+import UserNavbar from "@/components/layout/UserNavbar";
 
 export default function UserPage() {
   return (
     <div className="p-4 space-y-6">
-      <Navbar />
+      <UserNavbar />
       
       <WalletSection />
       <AjoSection />
