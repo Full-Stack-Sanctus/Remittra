8eabc4f08728f528d6322283330c874e1fc75d03
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-01-26 18:06:08 +0000

    Pushing commit

--- app/ajo/page.tsx
+++
@@ -1,9 +0,0 @@
-import AjoSection from "@/components/user/ajo/AjoSection";
-
-export default function AjoPage() {
-  return (
-    <div className="p-4">
-      <AjoSection />
-    </div>
-  );
-}
--- app/wallet/page.tsx
+++
@@ -1,9 +0,0 @@
-import WalletSection from "@/components/user/wallet/WalletSection";
-
-export default function WalletPage() {
-  return (
-    <div className="p-4">
-      <WalletSection />
-    </div>
-  );
-}
