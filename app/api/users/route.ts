a63c9b5f505ddea56a82a0cae45ae878eede9b95
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-02-25 01:07:45 +0000

    Pushing commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,20 @@
+// api/users/route.ts
+
 import { getSupabaseServer } from "@/lib/supabaseServerClient";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const supabaseServer = getSupabaseServer();
-  const { data } = await supabaseServer
+  
+  const supabaseServer = await getSupabaseServer(); 
+  
+  const { data, error } = await supabaseServer
     .from("users")
     .select("id, email, kyc_verified, verification_level");
+
+  if (error) {
+    console.error("Fetch error:", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
   return NextResponse.json(data || []);
 }
